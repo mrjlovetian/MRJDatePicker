4499e4177626901ca035a91b779763a06dd6c523
余洪江 <yuhongjiang642@tops001.com>
2018-05-14 12:23:05 +0800

    first commit

---
+++ NSDate+Helper.m
@@ -0,0 +1,461 @@
+//
+// NSDate+Helper.h
+//
+// Created by Billy Gray on 2/26/09.
+// Copyright (c) 2009, 2010, ZETETIC LLC
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the ZETETIC LLC nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY ZETETIC LLC ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL ZETETIC LLC BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import "NSDate+Helper.h"
+
+static NSCalendar *calendar;
+static NSDateFormatter *displayFormatter;
+
+@implementation NSDate (Helper)
+
++ (void)load { 
+    calendar =  [NSCalendar currentCalendar] ;
+    displayFormatter = [[NSDateFormatter alloc] init];
+     
+}
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+/*
+ * This guy can be a little unreliable and produce unexpected results,
+ * you're better off using daysAgoAgainstMidnight
+ */
+- (NSUInteger)daysAgo {
+    NSDateComponents *components = [calendar components:(NSDayCalendarUnit) 
+											   fromDate:self
+												 toDate:[NSDate date]
+												options:0];
+	return [components day];
+}
+
+- (NSUInteger)daysAgoAgainstMidnight {
+	// get a midnight version of ourself:
+	NSDateFormatter *mdf = [[NSDateFormatter alloc] init];
+	[mdf setDateFormat:@"yyyy-MM-dd"];  
+	NSDate *midnight = [mdf dateFromString:[mdf stringFromDate:self]];
+	//[mdf release];
+	
+	return (int)[midnight timeIntervalSinceNow] / (60*60*24) *-1;
+}
+
+- (NSString *)stringDaysAgo {
+	return [self stringDaysAgoAgainstMidnight:YES];
+}
+
+- (NSString *)stringDaysAgoAgainstMidnight:(BOOL)flag {
+	NSUInteger daysAgo = (flag) ? [self daysAgoAgainstMidnight] : [self daysAgo];
+	NSString *text = nil;
+	switch (daysAgo) {
+		case 0:
+			text = @"Today";
+			break;
+		case 1:
+			text = @"Yesterday";
+			break;
+		default:
+			text = [NSString stringWithFormat:@"%lu days ago", (unsigned long)daysAgo];
+	}
+	return text;
+}
+
+- (NSUInteger)weekday {
+    NSDateComponents *weekdayComponents = [calendar components:(NSWeekdayCalendarUnit) fromDate:self];
+	return [weekdayComponents weekday];
+}
+
+
+- (NSString *)weekDayName{
+    NSDateComponents *weekdayComponents = [calendar components:(NSWeekdayCalendarUnit) fromDate:self];
+    NSInteger  NSIntegerweekday = [weekdayComponents weekday]; // 星期几（注意，周日是“1”，周一是“2”。。。。）
+    
+    NSString *tempStr;
+    if(NSIntegerweekday == 1)
+    {
+        tempStr =@"Sunday".local_basic;
+    }
+    if(NSIntegerweekday == 2)
+    {
+        tempStr =@"Monday".local_basic;
+        
+    }
+    if(NSIntegerweekday == 3)
+    {
+        tempStr =@"Tuesday".local_basic;
+        
+    }
+    if(NSIntegerweekday == 4)
+    {
+        tempStr =@"Wednesday".local_basic;
+        
+    }
+    if(NSIntegerweekday == 5)
+    {
+        tempStr =@"Thursday".local_basic;
+        
+    }
+    if(NSIntegerweekday == 6)
+    {
+        tempStr =@"Friday".local_basic;
+        
+    }
+    if(NSIntegerweekday == 7)
+    {
+        tempStr =@"Saturday".local_basic;
+        
+    }
+    
+    return tempStr;
+
+}
+
+
+
+
+
+
++ (NSDate *)deserializeJsonDateString: (NSString *)jsonDateString
+{
+    NSInteger offset = [[NSTimeZone defaultTimeZone] secondsFromGMT]; //get number of seconds to add or subtract according to the client default time zone
+    
+    NSInteger startPosition = [jsonDateString rangeOfString:@"("].location + 1; //start of the date value
+    
+    NSTimeInterval unixTime = [[jsonDateString substringWithRange:NSMakeRange(startPosition, 13)] doubleValue] / 1000; //WCF will send 13 digit-long value for the time interval since 1970 (millisecond precision) whereas iOS works with 10 digit-long values (second precision), hence the divide by 1000
+    
+    NSDate* date =  [[NSDate dateWithTimeIntervalSince1970:unixTime] dateByAddingTimeInterval:offset];
+    
+    return date;
+}
+
+
+
+
+
++ (NSDate *)dateFromString:(NSString *)string {
+	return [NSDate dateFromString:string withFormat:[NSDate dbFormatString]];
+}
+
++ (NSDate *)dateFromString:(NSString *)string withFormat:(NSString *)format {
+	NSDateFormatter *inputFormatter = [[NSDateFormatter alloc] init];
+	[inputFormatter setDateFormat:format];
+	NSDate *date = [inputFormatter dateFromString:string];
+	//[inputFormatter release];
+	return date;
+}
+
++ (NSString *)stringFromDate:(NSDate *)date withFormat:(NSString *)format {
+	return [date stringWithFormat:format];
+}
+
++ (NSString *)stringFromDate:(NSDate *)date {
+	return [date string];
+}
+
++ (NSString *)stringForDisplayFromDate:(NSDate *)date prefixed:(BOOL)prefixed alwaysDisplayTime:(BOOL)displayTime
+{
+    /* 
+	 * if the date is in today, display 12-hour time with meridian,
+	 * if it is within the last 7 days, display weekday name (Friday)
+	 * if within the calendar year, display as Jan 23
+	 * else display as Nov 11, 2008
+	 */
+	
+	NSDate *today = [NSDate date];
+    NSDateComponents *offsetComponents = [calendar components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit) 
+													 fromDate:today];
+	
+	NSDate *midnight = [calendar dateFromComponents:offsetComponents];
+	
+	NSString *displayString = nil;
+	
+	// comparing against midnight
+	if ([date compare:midnight] == NSOrderedDescending) {
+		if (prefixed) {
+			[displayFormatter setDateFormat:@"'at' h:mm a"]; // at 11:30 am
+		} else {
+			[displayFormatter setDateFormat:@"h:mm a"]; // 11:30 am
+		}
+	} else {
+		// check if date is within last 7 days
+		NSDateComponents *componentsToSubtract = [[NSDateComponents alloc] init];
+		[componentsToSubtract setDay:-7];
+		NSDate *lastweek = [calendar dateByAddingComponents:componentsToSubtract toDate:today options:0];
+		//[componentsToSubtract release];
+		if ([date compare:lastweek] == NSOrderedDescending) {
+            if (displayTime)
+                [displayFormatter setDateFormat:@"EEEE h:mm a"]; // Tuesday
+            else
+                [displayFormatter setDateFormat:@"EEEE"]; // Tuesday
+		} else {
+			// check if same calendar year
+			NSInteger thisYear = [offsetComponents year];
+			
+			NSDateComponents *dateComponents = [calendar components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit) 
+														   fromDate:date];
+			NSInteger thatYear = [dateComponents year];			
+			if (thatYear >= thisYear) {
+                if (displayTime)
+                    [displayFormatter setDateFormat:@"MMM d h:mm a"];
+                else
+                    [displayFormatter setDateFormat:@"MMM d"];
+			} else {
+                if (displayTime)
+                    [displayFormatter setDateFormat:@"MMM d, yyyy h:mm a"];
+                else
+                    [displayFormatter setDateFormat:@"MMM d, yyyy"];
+			}
+		}
+		if (prefixed) {
+			NSString *dateFormat = [displayFormatter dateFormat];
+			NSString *prefix = @"'on' ";
+			[displayFormatter setDateFormat:[prefix stringByAppendingString:dateFormat]];
+		}
+	}
+	
+	// use display formatter to return formatted date string
+	displayString = [displayFormatter stringFromDate:date];
+	return displayString;
+}
+
++ (NSString *)stringForDisplayFromDate:(NSDate *)date prefixed:(BOOL)prefixed {
+    // preserve prior behavior
+	return [self stringForDisplayFromDate:date prefixed:prefixed alwaysDisplayTime:NO];
+}
+
++ (NSString *)stringForDisplayFromDate:(NSDate *)date {
+	return [self stringForDisplayFromDate:date prefixed:NO];
+}
+
+- (NSString *)stringWithFormat:(NSString *)format {
+	NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
+	[outputFormatter setDateFormat:format];
+	NSString *timestamp_str = [outputFormatter stringFromDate:self];
+	//[outputFormatter release];
+	return timestamp_str;
+}
+
+- (NSString *)string {
+	return [self stringWithFormat:[NSDate dbFormatString]];
+}
+
+- (NSString *)stringWithDateStyle:(NSDateFormatterStyle)dateStyle timeStyle:(NSDateFormatterStyle)timeStyle {
+	NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
+	[outputFormatter setDateStyle:dateStyle];
+	[outputFormatter setTimeStyle:timeStyle];
+	NSString *outputString = [outputFormatter stringFromDate:self];
+	//[outputFormatter release];
+	return outputString;
+}
+
+- (NSDate *)beginningOfWeek {
+	// largely borrowed from "Date and Time Programming Guide for Cocoa"
+	// we'll use the default calendar and hope for the best
+	
+    NSDate *beginningOfWeek = nil;
+	BOOL ok = [calendar rangeOfUnit:NSWeekCalendarUnit startDate:&beginningOfWeek
+						   interval:NULL forDate:self];
+	if (ok) {
+		return beginningOfWeek;
+	} 
+	
+	// couldn't calc via range, so try to grab Sunday, assuming gregorian style
+	// Get the weekday component of the current date
+	NSDateComponents *weekdayComponents = [calendar components:NSWeekdayCalendarUnit fromDate:self];
+	
+	/*
+	 Create a date components to represent the number of days to subtract from the current date.
+	 The weekday value for Sunday in the Gregorian calendar is 1, so subtract 1 from the number of days to subtract from the date in question.  (If today's Sunday, subtract 0 days.)
+	 */
+	NSDateComponents *componentsToSubtract = [[NSDateComponents alloc] init];
+	[componentsToSubtract setDay: 0 - ([weekdayComponents weekday] - 1)];
+	beginningOfWeek = nil;
+	beginningOfWeek = [calendar dateByAddingComponents:componentsToSubtract toDate:self options:0];
+	//[componentsToSubtract release];
+	
+	//normalize to midnight, extract the year, month, and day components and create a new date from those components.
+	NSDateComponents *components = [calendar components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit)
+											   fromDate:beginningOfWeek];
+	return [calendar dateFromComponents:components];
+}
+
+- (NSDate *)beginningOfDay {
+    // Get the weekday component of the current date
+	NSDateComponents *components = [calendar components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit) 
+											   fromDate:self];
+	return [calendar dateFromComponents:components];
+}
+
+- (NSDate *)endOfWeek {
+    // Get the weekday component of the current date
+	NSDateComponents *weekdayComponents = [calendar components:NSWeekdayCalendarUnit fromDate:self];
+	NSDateComponents *componentsToAdd = [[NSDateComponents alloc] init];
+	// to get the end of week for a particular date, add (7 - weekday) days
+	[componentsToAdd setDay:(7 - [weekdayComponents weekday])];
+	NSDate *endOfWeek = [calendar dateByAddingComponents:componentsToAdd toDate:self options:0];
+	//[componentsToAdd release];
+	
+	return endOfWeek;
+}
+//2013-04-01 16:14:28
+//2013-03-23 00:00:00
++ (NSString *)datetimeFormatString {
+	return @"yyyy-MM-dd HH:mm:ss";
+}
++ (NSString *)dateFormatString {
+	return @"yyyy-MM-dd";
+}
+
++ (NSString *)timeFormatString {
+	return @"HH:mm:ss";
+}
+
++ (NSString *)timestampFormatString {
+	return @"yyyy/MM/dd HH:mm:ss";
+}
+
+// preserving for compatibility
++ (NSString *)dbFormatString {	
+	return [NSDate timestampFormatString];
+}
+
+- (BOOL)isSameDay:(NSDate*)date1
+{
+    NSCalendar* calendar = [NSCalendar currentCalendar];
+    
+    unsigned unitFlags = NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit;
+    NSDateComponents* comp1 = [calendar components:unitFlags fromDate:date1];
+    NSDateComponents* comp2 = [calendar components:unitFlags fromDate:self];
+    
+    return [comp1 day]   == [comp2 day] &&
+    [comp1 month] == [comp2 month] &&
+    [comp1 year]  == [comp2 year];
+}
+
+- (NSDate *)destinationDateNow
+{
+    //设置源日期时区
+    NSDate *anyDate = self;
+    NSTimeZone* sourceTimeZone = [NSTimeZone timeZoneWithAbbreviation:@"UTC"];//或GMT
+    //设置转换后的目标日期时区,东八区
+    NSTimeZone* destinationTimeZone = [NSTimeZone timeZoneForSecondsFromGMT:8 * 3600];//[NSTimeZone localTimeZone];
+    //得到源日期与世界标准时间的偏移量
+    NSInteger sourceGMTOffset = [sourceTimeZone secondsFromGMTForDate:anyDate];
+    //目标日期与本地时区的偏移量
+    NSInteger destinationGMTOffset = [destinationTimeZone secondsFromGMTForDate:anyDate];
+    //得到时间偏移量的差值
+    NSTimeInterval interval = destinationGMTOffset - sourceGMTOffset;
+    //转为现在时间
+    NSDate* destinationDateNow = [[NSDate alloc] initWithTimeInterval:interval sinceDate:anyDate];
+    return destinationDateNow;
+}
+
++(NSDate *)dateFormServerTime:(NSString *)serverTime
+{
+    if (serverTime.length > 19) {
+        serverTime = [serverTime substringToIndex:19];
+    }
+    
+    ///系统时间
+    NSDate *date = [NSDate date];
+    
+    ///标准时区
+    NSTimeZone *standardTimeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
+    
+    ///手机系统时区
+    NSTimeZone *systemTimeZone = [NSTimeZone systemTimeZone];
+    
+    ///服务端时区, 默认是东八区
+    NSTimeZone *serverTimeZone = [NSTimeZone timeZoneForSecondsFromGMT:8*3600];
+    
+    ///系统时区和标准时间的差值
+    NSTimeInterval sysInter = [systemTimeZone secondsFromGMTForDate:date];
+    
+    ///服务端和标准时间的差值
+    NSTimeInterval serInter = [serverTimeZone secondsFromGMTForDate:date];
+    
+    ///服务端和手机的差值
+    NSTimeInterval counInter = sysInter - serInter;
+    
+    NSDateFormatter *formate = [[NSDateFormatter alloc] init];
+    formate.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+    [formate setTimeZone:standardTimeZone];
+    NSDate *serDate = [formate dateFromString:serverTime];
+    NSDate *newServerDate = [serDate dateByAddingTimeInterval:counInter];
+    return newServerDate;
+}
+
+///服务端时间转成本地时间，时区发生改变
++ (NSString *)handleServerTime:(NSString *)serverTime
+{
+    if (serverTime.length > 19) {
+        serverTime = [serverTime substringToIndex:19];
+    }
+    
+    ///系统时间
+    NSDate *date = [NSDate date];
+    
+    ///标准时区
+    NSTimeZone *standardTimeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
+    
+    ///手机系统时区
+    NSTimeZone *systemTimeZone = [NSTimeZone systemTimeZone];
+    
+    ///服务端时区, 默认是东八区
+    NSTimeZone *serverTimeZone = [NSTimeZone timeZoneForSecondsFromGMT:8*3600];
+    
+    ///系统时区和标准时间的差值
+    NSTimeInterval sysInter = [systemTimeZone secondsFromGMTForDate:date];
+    
+    ///服务端和标准时间的差值
+    NSTimeInterval serInter = [serverTimeZone secondsFromGMTForDate:date];
+    
+    ///服务端和手机的差值
+    NSTimeInterval counInter = sysInter - serInter;
+    
+    NSDateFormatter *formate = [[NSDateFormatter alloc] init];
+    formate.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+    [formate setTimeZone:standardTimeZone];
+    NSDate *serDate = [formate dateFromString:serverTime];
+    NSDate *newServerDate = [serDate dateByAddingTimeInterval:counInter];
+    NSString *phoneTime = [formate stringFromDate:newServerDate];
+    
+//    NSLog(@"手机显示时间是%@", phoneTime);
+    return phoneTime;
+}
+
+- (NSInteger)getTwoDateWithMonthFirstDate:(NSDate *)firstDate SecondDate:(NSDate *)secondDate
+{
+
+    NSCalendar *calender = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    //月
+    NSInteger month = kCFCalendarUnitMonth;
+    NSDateComponents *compsMonth = [calender components:month fromDate:firstDate toDate:secondDate options:0];
+    return [compsMonth month];
+}
+#pragma clang diagnostic pop
+@end
